a2ed567448a6e03c3b0e74670e438838d39898fd
yehongqing <13868303523@163.com>
2024-08-02 00:10:38 +0800

    fix demo

--- demo/test/unittest/mobilenetv2Test.cpp
+++ demo/test/unittest/mobilenetv2Test.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __ANDROID__
+#if !defined(__ANDROID__) && !defined(__MSVC__)
 #include <experimental/filesystem>
 #endif
 
